72ef1454b4931d221048e784dd59f44799fdf328
jkarage <josephbkarage@gmail.com>
2023-11-30 10:26:57 +0300

    Updated download api

--- download.go
+++ download.go
@@ -5,6 +5,8 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path"
+	"sync"
 )
 
 type App struct {
@@ -23,16 +25,18 @@ func NewApp(url string) *App {
 // Download uses single thread
 // downloads the file and saves it
 // in the file name provided
-func (app *App) Download(fn *os.File) error {
-	// fn, err := os.Create(path.Base(app.url))
-	// if err != nil {
-	// 	return err
-	// }
+func (app *App) Download() error {
+	fn, err := os.Create(path.Base(app.url))
+	if err != nil {
+		return err
+	}
 
 	resp, err := app.Get(app.url)
 	if err != nil {
 		return err
 	}
+
+	// defer resp.Body.Close() // No need, the server closes this.
 
 	if _, err = io.Copy(fn, resp.Body); err != nil {
 		return err
@@ -60,14 +64,38 @@ func Header(url string) (int64, string, bool, error) {
 		acceptRanges = true
 	}
 
-	fmt.Println(resp.Header)
-
 	return contentLength, eTag, acceptRanges, nil
 }
 
 // DownloadChunk gets chunks of bytes and assembles it.
 // to the file.
-func (app *App) DownloadChunk(fn *os.File, offset, size byte) error {
-	return nil
+func (app *App) DownloadChunks(f string, wg *sync.WaitGroup, start, end int) error {
+	defer wg.Done()
 
+	// Adds a Range Header to the request
+	r := fmt.Sprintf("bytes=%d-%d", start, end)
+
+	req, err := http.NewRequest(http.MethodGet, app.url, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Range", r)
+
+	resp, err := app.Client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(f, data, os.FileMode(os.O_APPEND)|os.FileMode(os.O_CREATE))
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
